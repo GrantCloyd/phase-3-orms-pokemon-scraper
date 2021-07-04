882e260355f257167be296d93d57abdfbaa09567
Grant Cloyd <dgcloyd@gmail.com>
2021-07-04 11:59:44 -0500

    commit tree cf5e40a497650225ece3b987aef569241f2bd1fd

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
+require 'pry'
+
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(attributes = {})
+        # @id = id
+        # @name = name
+        # @type = type
+        # @db = db
+       
+   attributes.each {|key, value| self.class.attr_accessor(key)} 
+   attributes.each {|key, value| self.send(("#{key}="), value)}
+    end
+    
+    def self.save(name, type, db) 
+     sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+     db.execute(sql, name, type)
+    end
+     
+
+
+    def self.find(id, db)
+    sql = <<-SQL
+        SELECT * 
+        from pokemon 
+        WHERE id = ?
+         SQL
+    id, name, type = db.execute(sql, id).flatten
+    hash = {:id => id, :name => name, :type => type}
+
+    self.new(hash)
+    
+    end
+
 end
